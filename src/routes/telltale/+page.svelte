fbf644bd0dc8fb8159477045546336cad7004a89
Frans Nilsson <franil@elevad.umea.se>
2022-12-12 11:30:20 +0100

    sdsfdfds

--- src/routes/telltale/+page.svelte
+++ src/routes/telltale/+page.svelte
@@ -25,42 +25,56 @@
         in:fly|local={{ x: 800, duration: 750 }}
         out:fly|local={{ x: -800, duration: 750 }}
       >
-        <!-- https://svelte.dev/tutorial/if-blocks -->
-        {#if story}
-          {#if story.img}
-            <img src={story.img} alt={story.message} />
+        <div class="center">
+          <!-- https://svelte.dev/tutorial/if-blocks -->
+          {#if story}
+            {#if story.img}
+              <img src={story.img} alt={story.message} />
+            {/if}
+            {#if story.html}
+              <!-- https://svelte.dev/tutorial/html-tags -->
+              {@html story.html}
+            {/if}
+            <article>
+              <span>{story.message}</span>
+            </article>
+            {#if story.question}
+              <!-- https://svelte.dev/tutorial/html-tags -->
+              <p>{story.question}</p>
+            {/if}
+            <div class="row">
+              <!-- https://svelte.dev/tutorial/each-blocks -->
+              {#each story.choices as choice}
+                <button
+                  on:click={() => {
+                    $story_id_store = choice.id;
+                  }}>{choice.text}</button
+                >
+              {/each}
+            </div>
+            <!-- https://svelte.dev/tutorial/else-blocks -->
+          {:else}
+            <p>STORY FOR ID {story_id} MISSING</p>
           {/if}
-          {#if story.html}
-            <!-- https://svelte.dev/tutorial/html-tags -->
-            {@html story.html}
-          {/if}
-          <p>{story.message}</p>
-          <div class="row">
-            <!-- https://svelte.dev/tutorial/each-blocks -->
-            {#each story.choices as choice}
-              <button
-                on:click={() => {
-                  $story_id_store = choice.id;
-                }}>{choice.text}</button
-              >
-            {/each}
-          </div>
-          <!-- https://svelte.dev/tutorial/else-blocks -->
-        {:else}
-          <p>STORY FOR ID {story_id} MISSING</p>
-        {/if}
-      </div>
+                </div>
+        </div>
     {/key}
   </div>
 </main>
 
 <style>
+  :global(body){
+    overflow: hidden;
+    height: 100%;
+
+  }
   .grid {
-    margin: 10%;
+    margin: 3%;
     display: grid;
     align-items: center;
     place-items: center;
     justify-content: center;
+    /* overflow: hidden; */
   }
 
   /* force the container to stay in the same position of the grid */
@@ -74,10 +88,31 @@
     grid-column-end: 2;
     grid-row-start: 1;
     grid-row-end: 2;
-    height: 400px;
+    height: 100%;
     width: 500px;
   }
+  .center{
+    display:flex;
+    flex-direction: column;
+    place-items: center;
+    place-content: center;
+    gap: 20px;
+  }
+  p{
+    text-align: center;
+    background-color: rgba(0, 0, 0, 0.1);
+    padding: 10px;
+    border-radius: 5px;
+    margin-bottom: 30px;
 
+  }
+  article{
+    text-align: center;
+    width: 80%;
+    background-color: rgba(0, 0, 0, 0.1);
+    padding: 10px;
+    border-radius: 5px;
+  }
   .row {
     display: flex;
     flex-direction: row;
@@ -85,9 +120,23 @@
   }
 
   img {
-    width: 400px;
-    height: 300px;
-    object-fit: cover;
+    width: 100%;
+    border-radius: 20px;
+  }
+
+  button{
+    background-color: rgba(0, 0, 0, 0.2);
+    padding: 10px;
+    border-radius: 5px;
+    border: none;
+  }
+
+  button:hover{
+    background-color: rgba(0, 0, 0, 0.3);
+  }
+
+  button:focus{
+    background-color: rgba(0, 0, 0, 0.5);
   }
 
   main {
